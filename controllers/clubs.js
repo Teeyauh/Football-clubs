858985d8ef429bf5abc6b8ae1faf8a246b8fe550
Teeyauh <60857288+Teeyauh@users.noreply.github.com>
2020-07-24 14:54:28 +0100

    prettier setup

--- controllers/clubs.js
+++ controllers/clubs.js
@@ -1,7 +1,7 @@
 class Clubs {
   static welcome(req, res, next) {
-    res.send({ message: "Welcome to Football Clubs Api" });
+    res.send({ message: 'Welcome to Football Clubs Api' })
   }
 }
 
-export default Clubs;
+export default Clubs
