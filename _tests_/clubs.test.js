b5bbc3fa2734883054851b19b496b2355140a321
Teeyauh <60857288+Teeyauh@users.noreply.github.com>
2020-08-04 23:36:36 +0100

    y

--- _tests_/clubs.test.js
+++ _tests_/clubs.test.js
@@ -11,14 +11,41 @@ const chai = require('chai')
 const should = chai.should()
 
 const request = supertest.agent(server)
-const ClubsModel = models.Clubs
+const clubsModel = models.Clubs
 
 describe('Football Clubs Api', () => {
+  before(async () => {
+    // create database tables
+    await models.sequelize.sync()
+
+    await clubsModel.create({
+      Name: 'Teeyauh FC.',
+      Stadium: 'Lokogoma',
+      Capacity: 70360,
+      Manager: 'Tayo',
+      Captain: 'Pierre',
+      country: 'Nigeria'
+    })
+  })
+  after(async () => {
+    // empty the database
+    await clubsModel.destroy({ where: {} })
+  })
   describe('Index route', () => {
     it('should return welcome message when /clubs route is matched', (done) => {
       request.get('/').end((err, res) => {
         res.status.should.be.equal(200)
         expect(res.body.message).be.equal('Welcome to football clubs Api')
+        done()
+      })
+    })
+  })
+  describe('Get all Clubs', () => {
+    it('it should GET all clubs', (done) => {
+      request.get('/clubs').end((err, res) => {
+        res.status.should.be.equal(200)
+        expect(res.body.clubs).to.be.an('array')
+        expect(res.body.message).be.equal('Clubs retrieved successfully')
         done()
       })
     })
