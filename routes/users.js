fce6de8aebdfef88843b585c48353e4b289c5043
Teeyauh <60857288+Teeyauh@users.noreply.github.com>
2020-08-08 01:03:11 +0100

    fixxx

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/extensions */
 import express from 'express'
 import User from '../controllers/users'
-import validateSignup from '../validators/validateSignup.js'
+// import validateSignup from '../validators/validateSignup.js'
 
 const router = express.Router()
 
-router.post('/signup', validateSignup, User.signUp)
+router.post('/signup', User.signUp)
 
 module.exports = router
